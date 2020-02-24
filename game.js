a78c7437278d3d1f9a5239d29f249941a985a713
ukoavl <33253904+ukoavl@users.noreply.github.com>
2020-02-24 13:22:26 +0000

    Update game.js

--- game.js
+++ game.js
@@ -9,4 +9,50 @@ function startGame() {
     showTextNode(30);
 }
 
+/**
+ * Show Text Node - Draw the text and images for the current room
+ * @param {number} textNodeIndex - The current room index
+ */
+function showTextNode(textNodeIndex) {
+    console.log(textNodeIndex);
+    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+    textElement.innerText = textNode.text
+
+
+    if(Object.keys(state)==textNode.picture.condition) {
+
+        console.log(state[textNode.picture.condition]);
+        if(state[textNode.picture.condition]){
+            picture.src = "./container/" + textNode.picture.default;
+        }
+    }else{
+        picture.src = "./container/" + textNode.picture.default; //images/ images folder
+        picture.title = textNode.pictureTitle;
+    }
+     
+
+
+
+while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild) // removing all buttons after loop
+}
+
+    console.log(optionButtonsElement.firstChild);
+    textNode.options.forEach(option => {
+
+            console.log(showOption(option))
+
+                if (showOption(option)) {
+
+                    const button = document.createElement('button')
+                    button.innerText = option.text
+
+                    button.classList.add('btn')
+                    button.style.title = "hello"
+                    button.addEventListener('click', () => selectOption(option))
+                    optionButtonsElement.appendChild(button)
+                }
+    })
+}
+
 
